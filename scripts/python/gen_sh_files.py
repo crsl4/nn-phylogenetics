3e3f3399cb72c779595cff49288906626d235395
Leonardo <lzepeda@math.mit.edu>
2021-02-16 14:23:46 -0600

    bug: fixed a bug in the naming convention

--- scripts/python/gen_sh_files.py
+++ scripts/python/gen_sh_files.py
@@ -2,7 +2,7 @@ import os
 
 name_script = "network_perm_equivariant_modular_conv_new_lba.py"
 
-string = "#!/usr/bin/env bash \n\n#SBATCH --partition=batch_default\n#SBATCH --cpus-per-task=2\n#SBATCH --gres=gpu:1\n#SBATCH --time=48:00:00\n\nsource ~/.bashrc\nmodule load anaconda/wml\n./bootstrap_conda\nmodule load cuda\n\nconda activate pytorch\n\nCUDA_VISIBLE_DEVICES=0 python "
+string = "#!/usr/bin/env bash \n\n#SBATCH --partition=batch_default\n#SBATCH --cpus-per-task=2\n#SBATCH --gres=gpu:1\n#SBATCH --time=48:00:00\n\nsource ~/.bashrc\nmodule load anaconda/wml\nbootstrap_conda\nmodule load cuda\n\nconda activate pytorch\n\nCUDA_VISIBLE_DEVICES=0 python "
 
 
 for ii in range(27):
