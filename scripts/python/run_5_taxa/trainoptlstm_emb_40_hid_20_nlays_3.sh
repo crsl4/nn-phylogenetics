46c2689c353e72a01c4a3260ebc70f6d10806cd9
Forgotten <lzepeda@math.mit.edu>
2021-12-27 16:42:08 -0600

    run: added ecample to run in cluster

---
+++ scripts/python/run_5_taxa/trainoptlstm_emb_40_hid_20_nlays_3.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#SBATCH --partition=research
+#SBATCH --time=1-1:0:0
+#SBATCH --mem 16000
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+
+module load anaconda/full/2021.05
+bootstrap_conda
+conda activate pytorch
+
+python ../network_zou/network_perm_eq_lstm_5_taxa_v2.py trainoptlstm_emb_40_hid_20_nlays_3.json
