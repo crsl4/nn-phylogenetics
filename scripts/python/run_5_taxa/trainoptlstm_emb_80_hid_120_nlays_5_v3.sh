21b4650be6c98dbd950b5075dad3dc53f8ae6dbc
Forgotten <lzepeda@math.mit.edu>
2021-12-28 23:47:54 -0600

    included large example

---
+++ scripts/python/run_5_taxa/trainoptlstm_emb_80_hid_120_nlays_5_v3.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#SBATCH --partition=research
+#SBATCH --time=2-1:0:0
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --constraint="ampere|turing"
+
+module load anaconda/full/2021.05
+bootstrap_conda
+conda activate pytorch
+
+python ../network_zou/network_perm_eq_lstm_5_taxa_v3.py trainoptlstm_emb_80_hid_120_nlays_5.json
